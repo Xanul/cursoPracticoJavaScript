b0494bbe59ab54d2f203f06cec137bfb6edfb671
Xanul <rivas.060@gmail.com>
2021-07-11 11:27:27 -0500

    First commit of the project, added the HTML and JS files

---
+++ figuras.js
@@ -0,0 +1,62 @@
+// Código del cuadrado
+console.group("Cuadrados");
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El área del cuadrado es de: " + areaCuadrado + "cm2");
+console.groupEnd();
+
+// Código del triangulo
+console.group("Triangulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+console.log(
+  "Los lados del triangulo miden " +
+    ladoTriangulo1 +
+    "cm, " +
+    ladoTriangulo2 +
+    "cm, " +
+    baseTriangulo +
+    "cm"
+);
+console.log("La altura del triangulo es " + alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+console.log("El perimetro del triangulo es: " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El área del triangulo es: " + areaTriangulo + "cm2");
+
+console.groupEnd();
+
+// Código del círculo
+console.group("Círculos");
+// Radio
+const radioCirculo = 4;
+console.log("El radio del circulo es: " + radioCirculo + "cm");
+
+// Diametro
+const diametroCirculo = radioCirculo * 2;
+console.log("El diametro del circulo es: " + diametroCirculo + "cm");
+
+// PI
+const PI = Math.PI;
+console.log("PI es: " + PI);
+
+// Circunferencia
+const perimetroCirculo = diametroCirculo * PI;
+console.log("El perimetro del circulo es: " + perimetroCirculo + "cm");
+
+// Área
+const areaCirculo = radioCirculo * radioCirculo * PI;
+console.log("El area del circulo es: " + areaCirculo + "cm2");
+
+console.groupEnd();
